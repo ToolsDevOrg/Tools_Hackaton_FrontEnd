39dc1785a00876e5d03827f78aa271f4cbfd63f8
texrode <arseniy.permyakov@gmail.com>
2025-05-22 20:12:08 +0500

    feat: register page

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,30 +1,33 @@
 import { ReactElement, ReactHTMLElement, ReactNode } from "react";
 import { TextInput, View } from "react-native";
 
-interface InputProps {
+export interface InputProps {
   children?: ReactNode;
   placeholder?: string;
-  type?: "text" | "number" | "FIO" | "email";
+  type?: "default" | "email-address" | "phone-pad" | "password";
+  value?: string;
+  onChange?: (text: string) => void;
 }
 
 export const CustomInput = ({
   children,
   placeholder,
-  type = "text",
+  type = "default",
+  value,
+  onChange,
 }: InputProps) => {
   return (
     <View className="border-[#F2F2F2] border-[0.8px] rounded-[20px] gap-[10px] p-[15px] flex-row">
       {children}
 
-      {type === "FIO" ? (
-        <>
-          {["Фамилия", "Имя", "Отчество"].map((place, index) => (
-            <TextInput key={index} placeholder={place} className="flex-1" />
-          ))}
-        </>
-      ) : (
-        <TextInput placeholder={placeholder} className=" h-[100%] w-[100%]" />
-      )}
+      <TextInput
+        placeholder={placeholder}
+        keyboardType={type === "password" ? "default" : type}
+        secureTextEntry={type === "password"}
+        className=" h-[100%] w-[100%]"
+        value={value}
+        onChangeText={onChange}
+      />
     </View>
   );
 };
