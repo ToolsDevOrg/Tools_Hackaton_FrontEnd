b5524221dcaacde9e82848aca20f4137fb2a8256
AlexeiZaits <zaykuh@gmail.com>
2025-05-23 04:24:41 +0500

    feat: feat

--- src/shared/ui/EventCard/EventCard.tsx
+++ src/shared/ui/EventCard/EventCard.tsx
@@ -69,7 +69,6 @@ export default function EventCard({
             resizeMode="cover"
           />
       </View>
-
       <View style={styles.info}>
         <Text style={styles.title}>{title}</Text>
         <View style={styles.location}>
