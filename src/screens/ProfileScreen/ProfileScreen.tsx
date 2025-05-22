d3ed0d0ec8a0fe3664a595ed1746d236f04fd5ef
texrode <arseniy.permyakov@gmail.com>
2025-05-23 02:27:16 +0500

    profile

---
+++ src/screens/ProfileScreen/ProfileScreen.tsx
@@ -0,0 +1,90 @@
+import { ScreenWrapper } from "@/shared/ui";
+import { Image, Pressable, Text, View } from "react-native";
+import { UjinLogo } from "../LoginScreen/icons";
+import { InfoIcon, JK, Phone, Logo, Logo2 } from "./icons";
+
+export const ProfileScreen = () => {
+  return (
+    <ScreenWrapper>
+      <View className="flex-row items-center justify-between w-[100%] py-[8px] mb-[10px]">
+        <View>
+          <Text className=" font-[700] text-[24px] leading-[36px]">
+            Профиль
+          </Text>
+          <Text className=" font-[600] text-[14px] leading-[24px] color-[#717171]">
+            Ваши личные данные
+          </Text>
+        </View>
+        <UjinLogo />
+      </View>
+      <View className="rounded-[20px] border-[#F2F2F5] border-[0.8px] bg-white w-[100%] p-[20px] gap-[12px] mb-[10px]">
+        <View className="flex-row items-center justify-between">
+          <View className="gap-[8px] max-w-[124px]">
+            <Text className="font-[700] text-[20px] leading-[30px]">
+              Алексей Феоктистов
+            </Text>
+            <View className="flex-row items-center gap-[8px]">
+              <JK />
+              <Text className="font-[600] text-[14px] leading-[24px] color-[#717171]">
+                ЖК “Премьер”
+              </Text>
+            </View>
+            <View className="flex-row items-center gap-[8px]">
+              <Phone />
+              <Text className="font-[600] text-[14px] leading-[24px] color-[#717171]">
+                +7 922 365 54 06
+              </Text>
+            </View>
+          </View>
+          <Image
+            className="w-[122px] h-[122px] rounded-[20px]"
+            source={require("./img/leha.jpg")}
+          />
+        </View>
+        <Pressable className="flex-row items-center w-[100%] justify-center rounded-[20px] bg-[#00C0C9] py-[10px] gap-[8px]">
+          <Text className="font-[600] text-[14px] leading-[24px] color-[#ffffff]">
+            Поддержка
+          </Text>{" "}
+          <InfoIcon />
+        </Pressable>
+      </View>
+      <View className="rounded-[20px] border-[#F2F2F5] border-[0.8px] bg-white p-[20px] overflow-hidden w-[100%] mb-[10px]">
+        <Text className="font-700 text-[18px] leading-[26px]">Мой дом</Text>
+        <Text className="font-600 text-[13px] leading-[21px] color-[#717171]">
+          Информация о вашей квартире
+        </Text>
+        <View className="absolute top-0 right-0">
+          <Logo2 />
+        </View>
+      </View>
+      <View className="rounded-[20px] border-[#F2F2F5] border-[0.8px] bg-white p-[20px] overflow-hidden w-[100%] mb-[10px]">
+        <Text className="font-700 text-[18px] leading-[26px]">Настройки</Text>
+        <Text className="font-600 text-[13px] leading-[21px] color-[#717171]">
+          Управление аккаунтом
+        </Text>
+        <View className="absolute top-0 right-0">
+          <Logo2 />
+        </View>
+      </View>
+      <View className="rounded-[20px] border-[#F2F2F5] border-[0.8px] bg-white p-[20px] overflow-hidden w-[100%] mb-[10px] items-start">
+        <Text className="font-700 text-[18px] leading-[26px]">
+          Нашли ошибку или улучшение?
+        </Text>
+        <Text className="font-600 text-[13px] leading-[21px] color-[#717171] pb-[8px]">
+          Мы вознаграждаем лучшие предложения или найденные ошибки
+        </Text>
+        <Pressable className="bg-[#00C0C9] py-[10px] px-[18px] rounded-[20px]">
+          <Text className="font-600 text-[13px] leading-[21px] color-[#ffffff]">
+            Попробовать
+          </Text>
+        </Pressable>
+        <View className="absolute bottom-0 right-0">
+          <Logo />
+        </View>
+      </View>
+      <Pressable className=" items-center w-[100%] justify-center rounded-[30px] bg-[#F1F1F1] py-[15px] gap-[8px]">
+        <Text className="font-[600] text-[16px]">Выйти</Text>
+      </Pressable>
+    </ScreenWrapper>
+  );
+};
