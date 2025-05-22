573a09cb02aa1a5eb6c85ba3a1a4f57df547cd6b
AlexeiZaits <zaykuh@gmail.com>
2025-05-22 20:52:54 +0500

    feat: add popup

--- src/screens/Menu1Screen.tsx
+++ src/screens/Menu1Screen.tsx
@@ -1,15 +1,19 @@
 import { useTypeNavigation } from '@/shared/hooks/useTypeNavigation';
 import { ScreenWrapper, Typography } from '@/shared/ui';
+import Popup from '@/widgets/popup/Popup';
 import { Pressable } from 'react-native';
 
 export const Menu1Screen: React.FC = () => {
   const navigation = useTypeNavigation();
 
   return (
+    <>
     <ScreenWrapper>
       <Pressable onPress={() => navigation.navigate('test1')}>
         <Typography color='white' fontSize={20}>Menu1</Typography>
       </Pressable>
     </ScreenWrapper>
+    <Popup/>
+    </>
   );
 };
