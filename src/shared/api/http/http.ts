b1f2b5e9a7fd1ccf0b53be19332e047546efe9b9
DMProductionn <goluzin.dima008@mail.ru>
2025-05-23 08:59:29 +0500

    login

--- src/shared/api/http/http.ts
+++ src/shared/api/http/http.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 // import { getToken } from "../token/Token";
 
 export const http = axios.create({
-  baseURL: 'http://172.20.10.11:8000',
-  withCredentials: true,
+  baseURL: 'http://172.20.10.2:8000',
 })
 
 // http.interceptors.request.use(
